e969429d5fc56a62d6b4cd1edf4be7eec086c2f7
dhananjaya5023 <dhananjaya5023@gmail.com>
2023-09-22 16:29:33 -0400

    Arthametic

---
+++ operater.js
@@ -0,0 +1,27 @@
+document.write("<h2> Delivery boys</h2>");
+document.getElementById("inner").innerHTML="earn daily 1000/-";
+
+document.write("Arthametic opearters");
+document.write("</br>")
+document.write("Raju- 180/- Ramu- 220/-");
+ 
+var raju=180;
+var ramu=220;
+document.write("<br/>")
+document.write("<br/>")
+document.write("<br/>")
+document.write("Ramu + Raju :"+(raju+ramu));
+document.write("<br/>")
+document.write("Ramu -Raju :"+(ramu-raju));
+document.write("<br/>")
+document.write("Ramu*Raju :"+(ramu*raju));
+document.write("<br/>")
+document.write("Ramu/Raju :"+(ramu/raju));
+document.write("<br/>")
+document.write("Ramu % Raju :"+(ramu%raju));
+document.write("<br/>")
+document.write("Ramu++ :"+(ramu++));
+document.write("<br/>")
+document.write("Ramu-- :"+(--ramu));
+document.write("<br/>")
+document.write("Ramu after decrement is:"+ramu);
